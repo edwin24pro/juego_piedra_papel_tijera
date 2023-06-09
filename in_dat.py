499a3bd2de3022f3f6c076ff31a5888971cfd9b4
Ivan AB <ivamh3r0@gmail.com>
2023-06-09 08:21:38 -0700

    fix

---
+++ in_dat.py
@@ -0,0 +1,8 @@
+#import random
+jugadorN1 = input('Nombre del primer jugador: ')
+jugadorN2 = input('Nombre del segundo jugador: ')
+
+
+opciones = ["piedra", "papel", "tijera"]
+
+#print(opciones[random.randint(0,2)])
--- juego_piedra_papel_tijera_utilizando_PA/in_dat.py
+++
@@ -1,2 +0,0 @@
-jugadorN1 = input('Escribe tu nombre: ')
-jugadorN2 = input('Escribe tu nombre: ')
